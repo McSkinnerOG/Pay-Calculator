c27e61b08eb331f4950a54952d072a2d1d627766
Va1idUser <40324085+McSkinnerOG@users.noreply.github.com>
2026-03-16 22:32:58 +1000

    Update service worker to version 5 with new files

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = "pay-calculator-v4";
+const CACHE_NAME = "pay-calculator-v5";
 
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/manifest.json",
   "/icon-192.png",
-  "/icon-512.png"
+  "/icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.2/jspdf.umd.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
 ];
 
 /* ---------- Install: pre-cache app shell ---------- */
@@ -40,7 +42,9 @@ self.addEventListener("fetch", event => {
     caches.open(CACHE_NAME).then(cache =>
       cache.match(event.request).then(cached => {
         const fetched = fetch(event.request).then(response => {
-          cache.put(event.request, response.clone());
+          if (response.ok) {
+            cache.put(event.request, response.clone());
+          }
           return response;
         }).catch(() => cached);
 
